16d488c29c066690f5f50cada94985f3fdb54853
Efe Yencilek <64659675+FarukEfe@users.noreply.github.com>
2024-09-28 17:38:07 -0400

    Debug 'char toFloat' in T4.c

--- A1/T4.c
+++ A1/T4.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 #include <math.h>
 
 #define SIGN_BIT 1
 #define BIASED_EXPONENT 3
 #define MANTISSA 4
 
-int convert_b2(int i) {
-    return 0;
-}
-
-char toFloat(float f) {
+// TASK 4.b
+char toFloat(float f, int silent) {
 
     // Extract decimal as positive
     float decimal = (f - (int)f)*((f >= 0) ? 1 : -1);
@@ -43,20 +41,44 @@ char toFloat(float f) {
     // We've already accounted for the negative, so always take positive value.
     final += (int)abs(f);
     printf("\n%f\n",final);
+    // Assign final value to character
+    char c = final;
     // If final value is out of ASCII range of 0 - 256, then display message and default to '0'.
-    if (final < 0 || final > 256) {
-        printf("Value out of ASCII range, defaulting to 0 as placeholder character...\n");
+    if (final < 0 || final > 255) {
+        printf((silent == 1) ? "" : "Value out of ASCII range, defaulting to 0 as placeholder character...\n");
+        return '0';
+    } else if ((int)c < 0) {
+        // Take precaution against multi-character character constant for unassigned ASCII characters
+        printf((silent == 1) ? "" : "Couldn't find a match with associated ascii, defaulting to 0...\n");
         return '0';
     }
-    char c = final;
+    // If no errors are apparent, return the computed character.
     return c;
+}
+
+// TASK 4.c
+void displayBits(char c) {
+    unsigned int ascii = c;
+    printf("Char: '%c' -- ASCII: %d -- Binary: ", c, ascii);
+
+    int i = sizeof(c) * CHAR_BIT;
+    // Convert ascii to bit according to the size of character
+    while (i--) {
+        printf ("%d ", (ascii >> i) & 0x1);
+    }
+    printf("\n");
 }
 
 int main(int argc, char const *argv[])
 {   
-    float myNum = 52.38;
-
-    char c = toFloat(myNum);
-    printf("%c\n",c);
+    // ASCII results with unassigned characters are meant to default to '0' as a character
+    float tests[10] = {
+        51.50, 0, 86.35, 43.25, 82.046, 62.59, 29.368, -34, -54.46, -300.569
+    };
+    for (int i=0;i<10;i++) {
+        float item = tests[i];
+        char c = toFloat(item,1);
+        displayBits(c);
+    }
     return 1;
 }
